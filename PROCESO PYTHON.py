c1b7b6873e50463f489d2cbe99375fa77c2dc76e
ROMANQR <111934915+ROMANQR@users.noreply.github.com>
2022-08-23 19:55:59 -0500

    Add files via upload

---
+++ PROCESO PYTHON.py
@@ -0,0 +1,18 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Hola, esta es una calculadora")
+	print("Dime el primer numero a sumar")
+	numeroa = input()
+	print("Dime el segundo numero a sumar")
+	numerob = input()
+	print("Dime el tercer numero a sumar")
+	numeroc = input()
+	resultado_suma = numeroa+numerob+numeroc
+	print("Resultado = ",resultado_suma)
+	prom = resultado_suma/3
+	print("El promedio es: ",prom)
+
